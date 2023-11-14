79958015200f8d5fdec71dac2866a6491737c191
veerabhadra swamy uppu <78792323+ultra-hash@users.noreply.github.com>
2023-11-14 10:30:23 +0530

    refactor - visual changes

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -8,84 +8,340 @@ class Board extends Component {
         return (
             <div className="board">
                 <div className="board-row">
-                    <div className="board-square black">1</div>
-                    <div className="board-square white">2</div>
-                    <div className="board-square black">3</div>
-                    <div className="board-square white">4</div>
-                    <div className="board-square black">5</div>
-                    <div className="board-square white">6</div>
-                    <div className="board-square black">7</div>
-                    <div className="board-square white">8</div>
+                    <div className="board-square white">
+                        <div className="top-left">8</div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
                 <div className="board-row">
-                    <div className="board-square white">1</div>
-                    <div className="board-square black">2</div>
-                    <div className="board-square white">3</div>
-                    <div className="board-square black">4</div>
-                    <div className="board-square white">5</div>
-                    <div className="board-square black">6</div>
-                    <div className="board-square white">7</div>
-                    <div className="board-square black">8</div>
+                    <div className="board-square black">
+                        <div className="top-left">7</div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
                 <div className="board-row">
-                    <div className="board-square black">1</div>
-                    <div className="board-square white">2</div>
-                    <div className="board-square black">3</div>
-                    <div className="board-square white">4</div>
-                    <div className="board-square black">5</div>
-                    <div className="board-square white">6</div>
-                    <div className="board-square black">7</div>
-                    <div className="board-square white">8</div>
+                    <div className="board-square white">
+                        <div className="top-left">6</div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
                 <div className="board-row">
-                    <div className="board-square white">1</div>
-                    <div className="board-square black">2</div>
-                    <div className="board-square white">3</div>
-                    <div className="board-square black">4</div>
-                    <div className="board-square white">5</div>
-                    <div className="board-square black">6</div>
-                    <div className="board-square white">7</div>
-                    <div className="board-square black">8</div>
+                    <div className="board-square black">
+                        <div className="top-left">5</div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
                 <div className="board-row">
-                    <div className="board-square black">1</div>
-                    <div className="board-square white">2</div>
-                    <div className="board-square black">3</div>
-                    <div className="board-square white">4</div>
-                    <div className="board-square black">5</div>
-                    <div className="board-square white">6</div>
-                    <div className="board-square black">7</div>
-                    <div className="board-square white">8</div>
+                    <div className="board-square white">
+                        <div className="top-left">4</div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
                 <div className="board-row">
-                    <div className="board-square white">1</div>
-                    <div className="board-square black">2</div>
-                    <div className="board-square white">3</div>
-                    <div className="board-square black">4</div>
-                    <div className="board-square white">5</div>
-                    <div className="board-square black">6</div>
-                    <div className="board-square white">7</div>
-                    <div className="board-square black">8</div>
+                    <div className="board-square black">
+                        <div className="top-left">3</div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
                 <div className="board-row">
-                    <div className="board-square black">1</div>
-                    <div className="board-square white">2</div>
-                    <div className="board-square black">3</div>
-                    <div className="board-square white">4</div>
-                    <div className="board-square black">5</div>
-                    <div className="board-square white">6</div>
-                    <div className="board-square black">7</div>
-                    <div className="board-square white">8</div>
+                    <div className="board-square white">
+                        <div className="top-left">2</div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
                 <div className="board-row">
-                    <div className="board-square white">1</div>
-                    <div className="board-square black">2</div>
-                    <div className="board-square white">3</div>
-                    <div className="board-square black">4</div>
-                    <div className="board-square white">5</div>
-                    <div className="board-square black">6</div>
-                    <div className="board-square white">7</div>
-                    <div className="board-square black">8</div>
+                    <div className="board-square black">
+                        <div className="top-left">1</div>
+                        <div></div>
+                        <div className="bottom-right">a</div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div className="bottom-right">b</div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div className="bottom-right">c</div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div className="bottom-right">d</div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div className="bottom-right">e</div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div className="bottom-right">f</div>
+                    </div>
+                    <div className="board-square black">
+                        <div></div>
+                        <div></div>
+                        <div className="bottom-right">g</div>
+                    </div>
+                    <div className="board-square white">
+                        <div></div>
+                        <div></div>
+                        <div className="bottom-right">h</div>
+                    </div>
                 </div>
             </div>
         )
